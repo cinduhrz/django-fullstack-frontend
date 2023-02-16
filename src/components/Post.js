98291207bf834661644ab2b74f02bc0670c3c348
Cindy Wong <cindywongdev@gmail.com>
2023-02-15 19:37:38 -0500

    Index functionality

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,5 +1,22 @@
+import { Link } from "react-router-dom"
+
 const Post = (props) => {
-    return <h1>Post</h1>
+    const post = props.post
+
+    // inline style
+    const divStyle = {
+        textAlign: "center",
+        border: "3px solid",
+        margin: "10px auto",
+        width: "80%"
+    }
+
+    return <div style={divStyle}>
+        <Link to={`/post/${post.id}`}>
+            <h1>{post.subject}</h1>
+        </Link>
+        <h2>{post.details}</h2>
+    </div>
 }
 
 export default Post
